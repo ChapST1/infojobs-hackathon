9955a7925d80f0965054fc583684dc2bbdffc45b
1TSpahc <3lprxd@gmail.com>
2023-05-29 18:06:55 -0500

    🎨🎨🎨

--- src/components/saveData/listOfCards/card/index.jsx
+++ src/components/saveData/listOfCards/card/index.jsx
@@ -25,15 +25,15 @@ export function Card ({ offer }) {
     <>
       {
         offer && (
-          <div className='w-full bg-[#7276b2] p-3 flex flex-col gap-2 rounded'>
+          <div className='w-full bg-[transparent] border-[4px] border-[#7276b2] p-3 flex flex-col gap-2 rounded'>
             <h4 className=' w-full text-base text-[#d5d7f7] text-1xl font-semibold py-[2px] whitespace-nowrap  overflow-hidden  text-ellipsis '>Puesto: <span className=' text-[#a8abd0]'>{offer?.title || DEFAULT_VALUE}</span></h4>
             <h4 className='text-base text-[#d5d7f7] text-1xl font-semibold py-[2px] whitespace-nowrap  overflow-hidden  text-ellipsis'>Salario: <span className='text-[#a8abd0]'>{offer?.salaryDescription || DEFAULT_VALUE}</span></h4>
             <h4 className='text-base text-[#d5d7f7] text-1xl font-semibold py-[2px] whitespace-nowrap  overflow-hidden  text-ellipsis'>Industria: <span className='text-[#a8abd0]'>{offer?.profile?.typeIndustry?.value || DEFAULT_VALUE}</span></h4>
             <h4 className='text-base text-[#d5d7f7] text-1xl font-semibold py-[2px] whitespace-nowrap  overflow-hidden  text-ellipsis'>Vacantes: <span className='text-[#a8abd0]'>{offer?.vacancies || DEFAULT_VALUE}</span></h4>
 
             <div className='flex gap-2'>
-              <button className=' flex-1 text-xs bg-[#434675] text-[#f0f3ff] py-3 px-3  text-center rounded duration-300 hover:bg-[#060608]' onClick={handleDetails}>Ver mas detalles</button>
-              <button className=' flex-1 text-xs bg-[#434675] text-[#f0f3ff] py-3 px-3  text-center rounded duration-300 hover:bg-[#060608]' onClick={handleDelete}>Eliminar</button>
+              <button className=' flex-1 text-xs bg-[#cfd2f5] text-[#565784] py-3 px-3  text-center rounded duration-300' onClick={handleDetails}>Ver mas detalles</button>
+              <button className=' flex-1 text-xs bg-[#f6d3e6] text-[#895b74] py-3 px-3  text-center rounded duration-300' onClick={handleDelete}>Eliminar</button>
             </div>
           </div>
         )
